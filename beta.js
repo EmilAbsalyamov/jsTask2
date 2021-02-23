a148b6c27cc7be174f5b831f1c45f8d1ea8c7412
EmilAbsalyamov <absalyamov.work@gmail.com>
2021-02-23 22:55:01 +0300

    version3

--- beta.js
+++ beta.js
@@ -3,13 +3,15 @@ function isPalindrome(data) {
    let letters = data.split('');
    for (let i = 0; i < letters.length; i++) {
       let first = letters[i]
-      for (let second = i + 1; second < letters.length;second++)
+      for (let second = i + 1; second < letters.length; second++) {
       
          if (first === letters[second]) {
             counter++;
-            console.log('счетчик= ' + counter + ' first= ' + first + ' sec= ' + letters[second])            
-         } 
+            console.log('счетчик= ' + counter + ' first= ' + first + ' sec= ' + letters[second])
+         }
       }
+   }
+   
    console.log(letters)
 } 
-isPalindrome('qweqwqw')
+isPalindrome('qwqwqw')
