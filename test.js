88474c1266cd6f6b5a2d607dc57a0189060e6c73
EmilAbsalyamov <absalyamov.work@gmail.com>
2021-03-07 14:03:54 +0300

    replCom

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-xzxzzx
+let people = [
+   Oleg = {
+   name: "Oleg",
+   lastname: "Pavlov",
+   age: 28,
+   },
+   Ibrahima = {
+      name: "Ibrahima",
+      lastname: "Balde",
+      age: 20,
+   },
+   Darell = {
+      name: "Darell",
+      lastname: "Tokpa",
+      age: 19,
+   },
+]
+let personInfo = (person) => console.log(person.name + " " + person.lastname + " " + person.age + " years old")
+for (let i = 0; i < people.length; i++){
+   if (people[i].age < 20) {
+      personInfo(people[i]);
+   }
+}
